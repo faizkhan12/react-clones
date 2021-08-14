5228415d1d0e0555d9752fc006a6ac4d8e83e526
faizkhan12 <faizk6797@gmail.com>
2021-08-14 17:53:37 +0530

    complete static amazon clone

--- amazon-clone/src/pages/index.js
+++ amazon-clone/src/pages/index.js
@@ -1,13 +1,32 @@
 import Head from "next/head"
+import Banner from "../components/Banner"
 import Header from "../components/Header"
+import ProductFeed from "../components/ProductFeed"
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   )
+}
+
+// making asyncronous call from next js server and returning it to component
+export async function getServerSideProps(context) {
+  const result = await fetch("https://fakestoreapi.com/products")
+  const products = await result.json()
+
+  // passing data from server to components
+  return {
+    props: {
+      products: products,
+    },
+  }
 }
