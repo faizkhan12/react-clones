5be23449ac8ffc644ed24eb85d9afcef2406cb78
faizkhan12 <faizk6797@gmail.com>
2021-12-04 21:02:10 +0530

    create spotify-clone

---
+++ spotify-clone/pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import Sidebar from "../components/Sidebar"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='bg-black h-screen overflow-hidden'>
+        <main>
+          {/* Sidebar */}
+          <Sidebar />
+          {/* Center */}
+        </main>
+        <div>{/* Player */}</div>
+      </div>
+    </div>
+  )
+}
---
+++ spotify-clone/pages/login.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Login = () => {
+  return <div>fddf</div>
+}
+
+export default Login
+
+export async function getServerSideProps() {}
