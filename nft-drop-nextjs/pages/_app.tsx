3c748b63f7ed1f3fd9f75f7c95b8160788fb43d2
faizkhan12 <faizk6797@gmail.com>
2022-07-03 14:46:04 +0530

    push nft drop collections site

---
+++ nft-drop-nextjs/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
+}
+
+export default MyApp
